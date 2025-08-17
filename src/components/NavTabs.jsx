3f84c5bf4a8b3887a1fde212de4f41bdfa34dfcf
subhinone <subhinone@gmail.com>
2025-08-17 22:38:03 +0900

    Update NavTabs.jsx

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -5,7 +5,7 @@ import "./NavTabs.css"
 const NavTabs = ({ activeTab, setActiveTab }) => {
   const tabs = [
     { label: "캘린더", key: "calendar" },
-    { label: "일정 추가", key: "plan" }, 
+    { label: "일정관리", key: "plan" }, 
     { label: "농사일지", key: "journal" },
     { label: "설정", key: "settings" },
   ];
